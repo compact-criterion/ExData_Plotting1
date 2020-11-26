bcab36dcfd14d331a046d49282ae88923dc9cdb6
Emmanuel Gonzaga <compact.criterion@gmail.com>
2020-11-26 20:35:48 +0800

    Add files via upload

---
+++ plot3.R
@@ -0,0 +1,19 @@
+library("data.table")
+
+setwd("~/Desktop/datasciencecoursera/4_Exploratory_Data_Analysis/project/data")
+powerdata <- data.table::fread(input = "household_power_consumption.txt"
+                               , na.strings="?"
+)
+powerdata[, Global_active_power := lapply(.SD, as.numeric), .SDcols = c("Global_active_power")]
+powerdata[, dateTime := as.POSIXct(paste(Date, Time), format = "%d/%m/%Y %H:%M:%S")]
+powerdata <- powerdata[(dateTime >= "2007-02-01") & (dateTime < "2007-02-03")]
+png("plot3.png", width=480, height=480)
+plot(powerdata[, dateTime], powerdata[, Sub_metering_1], type="l",
+     xlab="", ylab="Energy sub metering")
+lines(powerdata[, dateTime], powerdata[, Sub_metering_2],col="red")
+lines(powerdata[, dateTime], powerdata[, Sub_metering_3],col="blue")
+legend("topright"
+       , col=c("black","red","blue")
+       , c("Sub_metering_1  ","Sub_metering_2  ", "Sub_metering_3  ")
+       ,lty=c(1,1), lwd=c(1,1))
+dev.off()
